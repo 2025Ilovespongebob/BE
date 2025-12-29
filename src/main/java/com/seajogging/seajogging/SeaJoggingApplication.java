041536f126b1a7471f469a71c3c5fd03dcc45018
Yang jeongwoo <25_39@bssm.hs.kr>
2025-12-29 16:04:43 +0900

    초기화

---
+++ src/main/java/com/seajogging/seajogging/SeaJoggingApplication.java
@@ -0,0 +1,13 @@
+package com.seajogging.seajogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaJoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaJoggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seajogging/seajogging/SeaJoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seajogging.seajogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaJoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
